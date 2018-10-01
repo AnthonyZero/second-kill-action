80ee9f670a75fff8a254d26ea3f8418972fb7cb2
Anthony <736252868@qq.com>
2018-10-02 00:07:31 +0800

    seckill

---
+++ src/main/java/com/anthonyzero/seckill/Application.java
@@ -0,0 +1,11 @@
+package com.anthonyzero.seckill;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Application {
+    public static void main(String[] args) {
+        SpringApplication.run(Application.class, args);
+    }
+}
