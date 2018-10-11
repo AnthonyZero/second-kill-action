fa4f95c985532cad4e26b31b638f982a0da7a451
Anthony <736252868@qq.com>
2018-10-11 10:19:54 +0800

    :smile:

--- src/main/java/com/anthonyzero/seckill/config/WebConfig.java
+++ src/main/java/com/anthonyzero/seckill/config/WebConfig.java
@@ -3,6 +3,7 @@ package com.anthonyzero.seckill.config;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.method.support.HandlerMethodArgumentResolver;
+import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
 
 import java.util.List;
@@ -15,6 +16,9 @@ public class WebConfig extends WebMvcConfigurerAdapter {
 
     @Autowired
     private UserArgumentResolver userArgumentResolver;
+
+    @Autowired
+    private AccessInterceptor accessInterceptor;
     /**
      * 添加参数解析器
      * @param argumentResolvers
@@ -22,5 +26,14 @@ public class WebConfig extends WebMvcConfigurerAdapter {
     @Override
     public void addArgumentResolvers(List<HandlerMethodArgumentResolver> argumentResolvers) {
         argumentResolvers.add(userArgumentResolver);
+    }
+
+    /**
+     * 添加拦截器
+     * @param registry
+     */
+    @Override
+    public void addInterceptors(InterceptorRegistry registry) {
+        registry.addInterceptor(accessInterceptor);
     }
 }
